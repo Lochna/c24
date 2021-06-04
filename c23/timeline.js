6b05caaa67b8e1d84ad459eae25e3a6749c510d2
Lochna <84924278+Lochna@users.noreply.github.com>
2021-06-04 19:51:39 +0530

    Add files via upload

---
+++ c23/timeline.js
@@ -0,0 +1,23 @@
+var items=document.querySelector(".timeline li");
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+      console.log("one");
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+
+window.addEventListener("scroll",callbackFunc);
+window.addEventListener("load",callbackFunc);
